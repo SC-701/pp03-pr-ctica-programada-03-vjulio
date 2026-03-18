8644e9740711a29cf71ba009c2c7fd5997bbd385
Víctor Julio Rodríguez Cerdas <46943476+vjulio@users.noreply.github.com>
2026-03-17 23:09:28 -0600

    completada

---
+++ Vehiculo.WEB/Vehiculo.Web/Abstracciones/Interfaces/Reglas/IConfiguracion.cs
@@ -0,0 +1,7 @@
+﻿namespace Abstracciones.Interfaces.Reglas
+{
+    public interface IConfiguracion
+    {
+        string ObtenerMetodo(string seccion, string nombre);
+    }
+}
---
+++ Vehiculo.WEB/Vehiculo.Web/Abstracciones/Modelos/Marca.cs
@@ -0,0 +1,9 @@
+﻿
+namespace Abstracciones.Modelos
+{
+    public class Marca
+    {
+        public Guid Id { get; set; }
+        public string Nombre { get; set; }
+    }
+}
